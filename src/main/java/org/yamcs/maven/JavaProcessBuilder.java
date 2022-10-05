75594078147141211544ec4ca911f086ca8b758e
Fabian Diet <fabian.diet@spaceapplications.com>
2022-10-05 22:57:50 +0200

    Rework process shutdown

--- src/main/java/org/yamcs/maven/JavaProcessBuilder.java
+++ src/main/java/org/yamcs/maven/JavaProcessBuilder.java
@@ -1,18 +1,12 @@
 package org.yamcs.maven;
 
 import java.io.File;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
-import jnr.constants.platform.Signal;
-import jnr.posix.util.Platform;
-import jnr.posix.POSIX;
-import jnr.posix.POSIXFactory;
 
 import org.apache.maven.plugin.logging.Log;
 import org.codehaus.plexus.util.cli.StreamPumper;
@@ -46,52 +40,29 @@ public class JavaProcessBuilder {
         try {
             log.debug("Executing command: " + pb.command());
             process = pb.start();
-            Process reference = process;
-
-            // Get this early, because it could generate ClassNotFoundException during shutdown
-            POSIX posix = POSIXFactory.getNativePOSIX();
-            
-            Thread watchdog = new Thread(() -> {
-                if (reference != null && reference.isAlive()) {
-                    System.out.println("Gracefully stopping...");
-                    
-                    // Prefer not to use Process.destroy(), because it immediately shuts down
-                    // the process output stream, which may still provide useful information.
-                    long pid = getProcessPid(reference);
-                    if (pid != -1 && !Platform.IS_WINDOWS) {
-                        posix.kill(pid, Signal.SIGTERM.intValue());
-                    } else {
-                        reference.destroy();
-                    }
-                    try {
-                        boolean exited = reference.waitFor(stopTimeout, TimeUnit.MILLISECONDS);
-                        if (!exited) {
-                            System.out.println(String.format(
-                                "Yamcs did not stop in under %s milliseconds. Forcing...", stopTimeout));
-                            // This is also no "guarantee", but we did our best.
-                            reference.destroyForcibly();
-                        }
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                }
-            });
 
             if (stopTimeout >= 0) {
-                Runtime.getRuntime().addShutdownHook(watchdog);
+                Process reference = process;
+                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                    if (reference.isAlive()) {
+                        try {
+                            boolean exited = reference.waitFor(stopTimeout, TimeUnit.MILLISECONDS);
+                            if (!exited) {
+                                System.out.println(String.format(
+                                        "Yamcs did not stop in under %s milliseconds. Forcing...", stopTimeout));
+                                // This is also no "guarantee", but we did our best.
+                                reference.destroyForcibly();
+                            }
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }));
             }
 
             if (waitFor) {
                 redirectOutput(process, log);
                 process.waitFor();
-                if (!process.isAlive()) {
-                    try {
-                        Runtime.getRuntime().removeShutdownHook(watchdog);
-                    } catch (IllegalStateException e) {
-                        // Ignore: sporadically throws a "Shutdown in progress" exception,
-                        // which then shows a BUILD FAILURE in the Maven output.
-                    }
-                }
             }
 
             return process;
@@ -104,7 +75,7 @@ public class JavaProcessBuilder {
             Thread.currentThread().interrupt();
             return process;
         } catch (Exception e) {
-            if (process != null && process.isAlive()) {
+            if (process.isAlive()) {
                 process.destroy();
             }
             throw new Exception("Error running command: " + e.getMessage(), e);
@@ -121,24 +92,6 @@ public class JavaProcessBuilder {
         pb.directory(directory);
         pb.environment().putAll(extraEnv);
         return pb;
-    }
-
-    // This should work on linux and osx
-    private long getProcessPid(Process process) {
-        try {
-            // Java 9+
-            return process.pid();
-        } catch (NoSuchMethodError e) {
-            // Java 8 (on java 11 below code works, but generates reflection warnings)
-            Class<? extends Process> clazz = process.getClass();
-            try {
-                Field field = clazz.getDeclaredField("pid");
-                field.setAccessible(true);
-                return field.getInt(process);
-            } catch (NoSuchFieldException | IllegalAccessException e2) {
-                return -1;
-            }
-        }
     }
 
     public JavaProcessBuilder setArgs(List<String> argsList) {
